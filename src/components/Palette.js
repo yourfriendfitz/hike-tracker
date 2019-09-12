aad3a7053ecd3e95ed69b5092aed6a68b75ec87e
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-12 15:21:48 -0500

    added palette

---
+++ src/components/Palette.js
@@ -0,0 +1,20 @@
+const Primary = "#504B3A";
+const Secondary = "#EAB46D";
+const AltPrimary = "#4F431E";
+const AltSecondary = "#916F44";
+const Text = "#151613";
+const Light = "#AFBE8F";
+const Dark = "#505946";
+const Accent = "#2D2710";
+const AltAccent = "#564228";
+export {
+  Primary,
+  Secondary,
+  AltPrimary,
+  AltSecondary,
+  Text,
+  Light,
+  Dark,
+  Accent,
+  AltAccent
+};
